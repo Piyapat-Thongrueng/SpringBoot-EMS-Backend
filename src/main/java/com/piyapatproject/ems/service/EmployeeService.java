aa7506ade61ed73eefc537b7762661f433651024
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-07 16:46:08 +0700

    Build GET ALL Employee REST API

--- src/main/java/com/piyapatproject/ems/service/EmployeeService.java
+++ src/main/java/com/piyapatproject/ems/service/EmployeeService.java
@@ -2,9 +2,13 @@ package com.piyapatproject.ems.service;
 
 import com.piyapatproject.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
 
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
