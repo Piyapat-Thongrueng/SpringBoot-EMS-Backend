67520a82db2e1f13b40792d34bfeccd42bc31381
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-07 16:35:52 +0700

    Build GET Employee REST API

--- src/main/java/com/piyapatproject/ems/service/EmployeeService.java
+++ src/main/java/com/piyapatproject/ems/service/EmployeeService.java
@@ -5,4 +5,6 @@ import com.piyapatproject.ems.dto.EmployeeDto;
 public interface EmployeeService {
 
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
 }
--- src/main/java/com/piyapatproject/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/piyapatproject/ems/service/impl/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.piyapatproject.ems.service.impl;
 
 import com.piyapatproject.ems.dto.EmployeeDto;
 import com.piyapatproject.ems.entity.Employee;
+import com.piyapatproject.ems.exception.ResourceNotFoundException;
 import com.piyapatproject.ems.mapper.EmployeeMapper;
 import com.piyapatproject.ems.repository.EmployeeRepository;
 import com.piyapatproject.ems.service.EmployeeService;
@@ -20,5 +21,14 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(()->
+                        new ResourceNotFoundException("A requested employee does not exist with given id" + employeeId));
+
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
